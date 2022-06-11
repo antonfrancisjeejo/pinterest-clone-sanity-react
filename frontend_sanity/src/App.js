ffbcff319957e90d7dba73bb66d851ea0da683f3
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2022-06-11 22:51:49 +0530

    Initial commit

---
+++ frontend_sanity/src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import { Login } from "./components";
+import Home from "./container/Home";
+import { fetchUser } from "./utils/fetchUser";
+
+const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User = fetchUser();
+
+    if (!User) navigate("/login");
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
+};
+
+export default App;
